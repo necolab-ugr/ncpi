f2389bdef6f4c409696a6e498831af3b75ef5bcf
alejandroov <alexoroval@ugr.es>
2024-07-26 15:52:17 +0200

    Created test for EEG_topographic_plot

---
+++ tests/test_eeg_topographic_plot.py
@@ -0,0 +1,22 @@
+import sys 
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from ccpi.Analysis import Analysis
+
+import pandas as pd
+import numpy as np
+
+z_ratio = np.random.uniform(-10, 10, 19)
+p_value = np.random.uniform(0.03, 0.16, 19)
+group_arr = np.random.choice(['HCvsADMIL', 'HCvsADMOD'], 19)
+df = pd.DataFrame({'Group': group_arr, 'Ratio': z_ratio, 'p.value': p_value})
+
+def test_plot_df(df):
+    
+    ccpi_analysis = Analysis(df, group='HCvsADMIL', system=19, p_value=0.2, s=1.2, radius=1)
+    ccpi_analysis.EEG_topographic_plot(df,'HCvsADMIL', p_value=0.2, system=19)
+    # assert os.path.exists('EEG_tomography_plot.png')
+    # os.remove('EEG_tomography_plot.png')
+
+test_plot_df(df)
