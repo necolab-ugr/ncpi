5cf02b0430e265ceed1cc98784c4173d0c70207e
pablomc88 <pablomc@ugr.es>
2024-08-23 16:14:30 +0200

    Modified 'LFP_Chini_developing_brain' to incorporate the new functions of Features and Inference

--- examples/Hagen_model/LFP_Chini_developing_brain.py
+++ examples/Hagen_model/LFP_Chini_developing_brain.py
@@ -12,6 +12,7 @@ import scipy
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # ccpi toolbox
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
@@ -70,6 +71,8 @@ def load_empirical_data(folder_path):
     emp_data : dict
         Dictionary containing the loaded data. The keys are 'LFP', 'fs', and 'age'.
     '''
+
+    print(f'Loading files from {folder_path}')
     file_list = os.listdir(folder_path)
     emp_data = {'LFP': [], 'fs': [], 'age': []}
 
@@ -97,7 +100,7 @@ def compute_features(data, chunk_size=5., method='catch22'):
     data : dict
         Dictionary containing the LFP data.
     chunk_size : float
-        Size of the chunks in seconds.
+        Size of the chunks (epochs) in seconds.
     method : str
         Method used to compute the features.
 
@@ -107,7 +110,7 @@ def compute_features(data, chunk_size=5., method='catch22'):
         Pandas DataFrame containing the computed features.
     '''
 
-    # Split the data into chunks
+    # Split the data into chunks (epochs)
     chunk_size = int(chunk_size * data['fs'][0])
     chunked_data = []
     ID = []
@@ -136,6 +139,32 @@ def compute_features(data, chunk_size=5., method='catch22'):
     return df
 
 
+def compute_predictions(inference, data):
+    """
+    Compute predictions from the empirical data.
+
+    Parameters
+    ----------
+    inference : Inference
+        Inference object containing the trained model.
+    data : DataFrame
+        DataFrame containing the features of the empirical data.
+
+    Returns
+    -------
+    data : DataFrame
+        DataFrame containing the features of the empirical data and the predictions.
+    """
+
+    # Predict the parameters from the features of the empirical data
+    predictions = inference.predict(np.array(data['Features'].tolist()))
+
+    # Append the predictions to the DataFrame
+    pd_preds = pd.DataFrame({'Predictions': predictions})
+    data = pd.concat([data, pd_preds], axis=1)
+
+    return data
+
 if __name__ == "__main__":
     # Load the configuration file that stores all file paths used in the script
     with open('config.json', 'r') as config_file:
@@ -152,6 +181,11 @@ if __name__ == "__main__":
     print(f'Samples loaded: {len(theta["data"])}')
     print(f'Done in {end_time - start_time} s')
 
+    # Randomly subsample the simulation data
+    idx = np.random.choice(len(theta['data']), 100000, replace=False)
+    X = X[idx]
+    theta['data'] = theta['data'][idx]
+
     # Load empirical data
     print('\n--- Loading empirical data.')
     start_time = time.time()
@@ -166,3 +200,60 @@ if __name__ == "__main__":
     emp_data = compute_features(emp_data, chunk_size=5., method='catch22')
     end_time = time.time()
     print(f'Done in {end_time - start_time} s')
+
+    # Create the Inference object, add the simulation data and train the model
+    print('\n--- Training the regression model.')
+    start_time = time.time()
+    model = 'MLPRegressor'
+    hyperparams = {'hidden_layer_sizes': (100,100), 'max_iter': 1000, 'tol': 1e-2, 'n_iter_no_change': 4,
+                   'verbose': True}
+    inference = ccpi.Inference(model=model, hyperparams=hyperparams)
+    inference.add_simulation_data(X, theta['data'])
+    inference.train(param_grid=None)
+    end_time = time.time()
+    print(f'Done in {end_time - start_time} s')
+
+    # Compute predictions from the empirical data
+    print('\n--- Computing predictions from empirical data.')
+    start_time = time.time()
+    emp_data = compute_predictions(inference, emp_data)
+    end_time = time.time()
+    print(f'Done in {end_time - start_time} s')
+
+    # Replace parameters of recurrent synaptic conductances with the ratio (E/I)_net
+    E_I_net = emp_data['Predictions'].apply(lambda x: (x[0]/x[2]) / (x[1]/x[3]))
+    others = emp_data['Predictions'].apply(lambda x: x[4:])
+    emp_data['Predictions'] = (np.concatenate((E_I_net.values.reshape(-1,1),
+                                               np.array(others.tolist())), axis=1)).tolist()
+
+    # Save the predictions
+    if not os.path.exists('data'):
+        os.makedirs('data')
+    emp_data.to_pickle('data/emp_data.pkl')
+
+    # Plot predictions as a function of age
+    plt.figure(dpi = 300)
+    plt.rc('font', size=8)
+    plt.rc('font', family='Arial')
+    titles = [r'$(E/I)_{net}$', r'$\tau_{exc}^{syn}$', r'$\tau_{inh}^{syn}$', r'$J_{ext}^{syn}$']
+
+    for param in range(4):
+        plt.subplot(1,4,param+1)
+        param_pd = pd.DataFrame({'Group': emp_data['Group'],
+                                 'Predictions': emp_data['Predictions'].apply(lambda x: x[param])})
+        ax = sns.boxplot(x='Group', y='Predictions', data=param_pd, showfliers=False,
+                         palette='Set2', legend=False, hue='Group')
+        ax.set_title(titles[param])
+        if param == 0:
+            ax.set_ylabel('Predictions')
+        else:
+            ax.set_ylabel('')
+        ax.set_xlabel('Postnatal days')
+        ax.set_xticks(np.arange(0, len(np.unique(emp_data['Group'])), 2))
+        ax.set_xticklabels([f'P{str(i)}' for i in np.unique(emp_data['Group'])[::2]])
+        plt.tight_layout()
+
+    # Save the plot
+    if not os.path.exists('figures'):
+        os.makedirs('figures')
+    plt.savefig('figures/predictions.png')
