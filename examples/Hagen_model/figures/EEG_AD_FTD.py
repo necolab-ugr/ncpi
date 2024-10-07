c478849b5fdc3fe13fb8917cc34407cec9dfddef
alejandroov <alexoroval@ugr.es>
2024-10-07 08:55:45 +0200

    Added parameters to control figures in EEG_AD_FTD.py

--- examples/Hagen_model/figures/EEG_AD_FTD.py
+++ examples/Hagen_model/figures/EEG_AD_FTD.py
@@ -8,9 +8,12 @@ import matplotlib.patches as mpatches
 from matplotlib.collections import PatchCollection
 from matplotlib.cm import ScalarMappable
 from matplotlib import colorbar
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.lines as mlines
+# EEG_AD_FTD_path = '/DATOS/pablomc/EEG_AD_FTD_results'
+EEG_AD_FTD_path = '/home/alejandro/CCPI/DATA/features'
 
-EEG_AD_FTD_path = '/DATOS/pablomc/EEG_AD_FTD_results'
-n_var = 1
+n_var = 2
 
 all_confs = ['catch22',
             'SC_FluctAnal_2_rsrangefit_50_1_logi_prop_r1',
@@ -30,35 +33,33 @@ def plot_simple_head_model(ax, radius, pos):
     pos: float
         Position of the head on the x-axis.
     '''
-    # Ears
-    patches = []
-    right_ear = mpatches.FancyBboxPatch([pos + radius + radius / 20, -radius/10],
-                                        radius/50, radius/5,
-        boxstyle=mpatches.BoxStyle("Round", pad=radius/20))
-    patches.append(right_ear)
 
-    left_ear = mpatches.FancyBboxPatch([pos -radius - radius / 20 - radius / 50,
-                                        -radius / 10],radius / 50, radius / 5,
-        boxstyle=mpatches.BoxStyle("Round", pad=radius/20))
+    ax.set_aspect('equal')
 
-    patches.append(left_ear)
-    collection = PatchCollection(patches, facecolor='none',
-                                 edgecolor='k', alpha=1.0, lw=2)
-    ax.add_collection(collection)
 
-    # Circumference of the head
-    circ_npts = 100
-    head_x = pos + radius * np.cos(np.linspace(0, 2 * np.pi, circ_npts))
-    head_y = radius * np.sin(np.linspace(0, 2 * np.pi, circ_npts))
+    # Cabeza
+    head_circle = mpatches.Circle((pos, 0), radius+0.02, edgecolor='k', facecolor='none', linewidth=0.5)
+    ax.add_patch(head_circle)
 
-    ax.plot(head_x, head_y, 'k')
-    ax.plot([radius])
+    # Orejas
+    right_ear = mpatches.FancyBboxPatch([pos + radius + radius / 20, -radius / 10],
+                                        radius / 50, radius / 5,
+                                        boxstyle=mpatches.BoxStyle("Round", pad=radius / 20),
+                                        linewidth=0.5)
+    ax.add_patch(right_ear)
 
-    # Nose
-    ax.plot([pos -radius / 10, pos, pos + radius  / 10], [radius,
-            radius + radius/10, radius], 'k')
+    left_ear = mpatches.FancyBboxPatch([pos - radius - radius / 20 - radius / 50, -radius / 10],
+                                       radius / 50, radius / 5,
+                                       boxstyle=mpatches.BoxStyle("Round", pad=radius / 20),
+                                       linewidth=0.5)
+    ax.add_patch(left_ear)
 
-def plot_EEG(fig, Vax, data, radius, pos, vmin, vmax):
+    # Nariz
+    ax.plot([pos - radius / 10, pos, pos + radius / 10], 
+            [radius + 0.02, radius + radius / 10 + 0.02,0.02 + radius], 
+            'k', linewidth=0.5)
+
+def plot_EEG(fig, Vax, data, radius, pos, vmin, vmax, label=True):
     '''
     Plot slopes or E/I predictions on EEG electrodes (20 EEG montage) as a
     contour plot.
@@ -133,32 +134,46 @@ def plot_EEG(fig, Vax, data, radius, pos, vmin, vmax):
     # Use different number of levels for the fill and the lines
     CS = Vax.contourf(xi, yi, zi, 30, cmap = plt.cm.bwr, zorder = 1,
                       vmin = vmin,vmax = vmax)
-    Vax.contour(xi, yi, zi, 5, colors = "grey", zorder = 2,linewidths = 0.5,
+    Vax.contour(xi, yi, zi, 5, colors = "grey", zorder = 2,linewidths = 0.4,
                 vmin = vmin,vmax = vmax)
 
     # Make a color bar
     # cbar = fig.colorbar(CS, ax=Vax)
-    fig.colorbar(ScalarMappable(norm=CS.norm, cmap=CS.cmap), ax = Vax)
+    divider = make_axes_locatable(Vax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+
+    if np.sum(np.abs(data)) > 2: 
+        colorbar = fig.colorbar(ScalarMappable(norm=CS.norm, cmap=CS.cmap), cax=cax)
+        colorbar.ax.tick_params(labelsize=5)
+        if label == True:
+            colorbar.ax.xaxis.set_label_position('top')
+            bbox = colorbar.ax.get_position()
+            # print(bbox)
+            colorbar.set_label('Ratio', size=5, labelpad=0, rotation=270, va='center')
+            
+    else:
+        # Hide the colorbar if the data is not significant
+        cax.axis('off')
 
     # Add the EEG electrode positions
-    Vax.scatter(x[:20], y[:20], marker = 'o', c = 'k', s = 2, zorder = 3)
+    Vax.scatter(x[:20], y[:20], marker = 'o', c = 'k', s = 0.5, zorder = 3)
 
 
 if __name__ == "__main__":
     # Load features
     # POCTEP dataset
     # emp_data_POCTEP_source = pd.read_pickle(os.path.join(EEG_AD_FTD_path, 'catch22', 'emp_data_POCTEP_False.pkl'))
-    emp_data_POCTEP_raw = pd.read_pickle(os.path.join(EEG_AD_FTD_path, 'catch22', 'emp_data_POCTEP_True.pkl'))
+    # emp_data_POCTEP_raw = pd.read_pickle(os.path.join(EEG_AD_FTD_path, 'catch22', 'emp_data_POCTEP_True.pkl'))
     # OpenNEURO dataset
-    emp_data_OpenNeuro = pd.read_pickle(os.path.join(EEG_AD_FTD_path, 'catch22', 'emp_data_OpenNEURO.pkl'))
+    # emp_data_OpenNeuro = pd.read_pickle(os.path.join(EEG_AD_FTD_path, 'catch22', 'emp_data_OpenNEURO.pkl'))
 
     # Load LMER results
-    lmer_feat = pickle.load(open(os.path.join(EEG_AD_FTD_path, 'catch22', 'lmer_feat.pkl'), 'rb'))
-    lmer_preds = pickle.load(open(os.path.join(EEG_AD_FTD_path, 'catch22', 'lmer_preds.pkl'), 'rb'))
+    lmer_feat = pickle.load(open(os.path.join(EEG_AD_FTD_path, 'databases', 'lmer_feat.pkl'), 'rb'))
+    lmer_preds = pickle.load(open(os.path.join(EEG_AD_FTD_path, 'databases', 'lmer_preds.pkl'), 'rb'))
 
     # Fig. 2
-    fig2 = plt.figure(figsize=(7.5, 3.5), dpi=300)
-    plt.rcParams.update({'font.size': 10, 'font.family': 'Arial'})
+    # fig2 = plt.figure(figsize=(7.5, 3.5), dpi=300)
+    plt.rcParams.update({'font.family': 'Arial'})
 
     # Define 4 colors for DB1
     colors_1 = ['lightgrey', 'lightcoral', 'lightblue', 'lightgreen']
@@ -266,21 +281,30 @@ if __name__ == "__main__":
 
             # Titles
             ax.set_title(f'DB{1 if col%2 == 0 else 2}')
+    sizex = 3.5
+    sizey = 4
+    left = 0.03
+    right = 0.17
+    width = (1.0 - left - right) / (2 * 5) - 0.037  
+    height = 1.0 / 4 + 0.06
+    bottom = 1 - height 
+    spacing_x = 0.14
+    new_spacing_x = 0.16
+    spacing_y = -0.095
 
-
-    # Fig. 3
-    fig3 = plt.figure(figsize=(7.5, 4.), dpi=300)
-
+    # # Fig. 3
+    fig3 = plt.figure(figsize=(4.5, 5), dpi=300)
+    current_bottom = bottom
     for row in range(4):
+        current_left = left
         for col in range(5):
-            ax = fig3.add_axes([0.05 + col * 0.19, 0.72 - row * 0.23, 0.15, 0.2])
 
             if row == 0:
                 feat = 18
             if row == 1:
-                feat = 8
-            if row == 2:
                 feat = 4
+            if row == 2:
+                feat = 8
             if row == 3:
                 feat = 19
 
@@ -295,6 +319,11 @@ if __name__ == "__main__":
             if col == 4:
                 group = 'A'
 
+            ax = fig3.add_axes([current_left, current_bottom, 0.15, 0.15], frameon=False)
+            if col == 2:
+                    current_left += width + new_spacing_x
+            else:
+                current_left += width + spacing_x
             # Get lmer results
             if col < 3:
                 lmer_results = lmer_feat[1]['DB1_raw'][f'{feat}']
@@ -311,18 +340,25 @@ if __name__ == "__main__":
                     data.append(0)
 
             # Plot EEG
-            plot_simple_head_model(ax, 1, 0)
+            plot_simple_head_model(ax, 0.6, 0)
             # plot_EEG(fig3, ax, data, 1, 0, -np.max(np.abs(data)), np.max(np.abs(data)))
 
             if col < 3:
                 if row == 0:
-                    ylims = [-12,12]
-                else:
+                    ylims = [-11,11]
+                if row == 1 or row == 2:
                     ylims = [-6,6]
+                if row == 3:
+                    ylims = [-3.5, 3.5]
             else:
-                ylims = [-6, 6]
+                if row == 0 or row == 1 or row == 3:
+                    ylims = [-6,6]
+                if row == 2:
+                    ylims = [-3.5, 3.5]
 
-            plot_EEG(fig3, ax, data, 1, 0, ylims[0], ylims[1])
+            
+
+            plot_EEG(fig3, ax, data, 0.6, 0, ylims[0], ylims[1])
 
             # ticks
             ax.set_xticks([])
@@ -330,27 +366,52 @@ if __name__ == "__main__":
 
             # Titles
             if row == 0:
-                ax.set_title(f'{group}vsHC')
+                ax.set_title(f'{group} vs HC', fontsize=8)
 
             # Labels
             if col == 0:
                 if row == 0:
-                    ax.set_ylabel(r'$rs\_range$')
+                    ax.set_ylabel(r'$rs\_range$', fontsize=5)
                 if row == 1:
-                    ax.set_ylabel(r'$TransVar$')
+                    ax.set_ylabel(r'$ami2$',fontsize=5)
                 if row == 2:
-                    ax.set_ylabel(r'$ami2$')
+                    ax.set_ylabel(r'$TransVar$',fontsize=5)
                 if row == 3:
-                    ax.set_ylabel(r'$dfa$')
+                    ax.set_ylabel(r'$dfa$', fontsize=5)
+        current_bottom -= height + spacing_y
+    
+    fig3.text(0.29, 0.95, 'DB1', ha='center', fontsize=8)
+    fig3.text(0.8, 0.95, 'DB2', ha='center', fontsize=8)
+    linepos = [0.94, 0.94]
+    line1 = mlines.Line2D([0.013, 0.57], linepos, color='black', linewidth=0.7)
+    line2 = mlines.Line2D([0.62, 0.95], linepos, color='black', linewidth=0.7)
+    fig3.add_artist(line1)
+    fig3.add_artist(line2)
+    
+    height = 1.0 / 5
+    sizex = 3.5
+    sizey = 4
+    left = 0.03
+    right = 0.17
+    width = (1.0 - left - right) / (2 * 5) - 0.037  
+    height = 1.0 / 4 
+    bottom = 1 - height - 0.03
+    spacing_x = 0.15
+    new_spacing_x = 0.16
+    spacing_y = -0.08
 
+    max = 0
+    yname = [r'$[E/I]_{net}$', r'$J_{ext}$']
+    order = [1,3,2,4,0]
     # Figs. 5 & 6
     for param in range(n_var):
-        fig = plt.figure(figsize=(7.5, 5.), dpi=300)
-
-        for row in range(5):
+        fig = plt.figure(figsize=(4.5, 5), dpi=300)
+        current_bottom = bottom
+        for row in order:
+            current_left = left
             for col in range(5):
-                ax = fig.add_axes([0.05 + col * 0.19, 0.8 - row * 0.18, 0.15, 0.15])
 
+                
                 if col == 0:
                     group = 'ADMIL'
                 if col == 1:
@@ -361,6 +422,15 @@ if __name__ == "__main__":
                     group = 'F'
                 if col == 4:
                     group = 'A'
+                
+                
+                ax = fig.add_axes([current_left, current_bottom, 0.15, 0.15], frameon=False)
+                if col == 2:
+                    current_left += width + new_spacing_x
+                else:
+                    current_left += width + spacing_x
+                # ax = fig.add_axes([0.05 + col * 0.19, 0.75 - row * 0.18, 0.15, 0.15])
+
 
                 # Get lmer results
                 if col < 3:
@@ -372,35 +442,47 @@ if __name__ == "__main__":
                 for elec in range(19):
                     p_value = lmer_results[f'{group}vsHC']['p.value'][elec]
                     z_score = lmer_results[f'{group}vsHC']['z.ratio'][elec]
-                    if p_value < 0.01 and np.abs(z_score) > 2.5:
+                    if p_value < 0.01:
                         data.append(z_score)
                     else:
                         data.append(0)
-
+                range_max = np.max(np.abs(data))
+                ylims = [-6, 6]
+                if row == 1 and col == 0:
+                    vmin = -4.720872348422581
+                    vmax = 4.720872348422581
                 # Plot EEG
-                plot_simple_head_model(ax, 1, 0)
-                plot_EEG(fig3, ax, data, 1, 0, -np.max(np.abs(data)), np.max(np.abs(data)))
+                plot_simple_head_model(ax, 0.6, 0)
+                plot_EEG(fig, ax, data, 0.6, 0, ylims[0], ylims[1])
 
                 # ticks
                 ax.set_xticks([])
                 ax.set_yticks([])
 
                 # Titles
-                if row == 0:
-                    ax.set_title(f'{group}vsHC')
+                if row == 1:
+                    ax.set_title(f'{group} vs HC', fontsize=8)
 
                 # Labels
                 if col == 0:
                     if row == 0:
-                        ax.set_ylabel(r'$catch22$')
+                        ax.set_ylabel(f'{yname[param]}'r'($catch22$)', fontsize=5)
                     if row == 1:
-                        ax.set_ylabel(r'$rs\_range$')
+                        ax.set_ylabel(f'{yname[param]}'r'($rs\_range$)', fontsize=5)
                     if row == 2:
-                        ax.set_ylabel(r'$TransVar$')
+                        ax.set_ylabel(f'{yname[param]}'r'($TransVar$)', fontsize=5)
                     if row == 3:
-                        ax.set_ylabel(r'$ami2$')
+                        ax.set_ylabel(f'{yname[param]}'r'($ami2$)', fontsize=5)
                     if row == 4:
-                        ax.set_ylabel(r'$dfa$')
+                        ax.set_ylabel(f'{yname[param]}'r'($dfa$)', fontsize=5)
+            current_bottom -= height + spacing_y
 
+        fig.text(0.29, 0.95, 'DB1', ha='center', fontsize=8)
+        fig.text(0.79, 0.95, 'DB2', ha='center', fontsize=8)
+        linepos = [0.94, 0.94]
+        line1 = mlines.Line2D([0.03, 0.56], linepos, color='black', linewidth=0.7)
+        line2 = mlines.Line2D([0.63, 0.93], linepos, color='black', linewidth=0.7)
+        fig.add_artist(line1)
+        fig.add_artist(line2)
 
     plt.show()
