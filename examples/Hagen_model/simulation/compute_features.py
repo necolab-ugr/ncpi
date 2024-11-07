Fix memory issue in EEG computation by optimizing storage NYheadmodel and EEG structures.

examples/Hagen_model/simulation/compute_features.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
 import ncpi
 
 # Set to True if features should be computed for the EEG data instead of the CDM data
-compute_EEG = False
+compute_EEG = True
 
 if __name__ == '__main__':
     # Path to the folder containing the processed data
@@ -49,6 +49,8 @@ if __name__ == '__main__':
                 # CDM data
                 if file[:3] == 'CDM':
                     CDM_data = pickle.load(open(os.path.join(sim_file_path,file),'rb'))
+                    # TO REMOVE!
+                    # CDM_data = CDM_data[:10,:]
 
                     # Split CDM data into 10 chunks when computing EEGs to avoid memory issues
                     if compute_EEG:
@@ -57,29 +59,28 @@ if __name__ == '__main__':
                         all_CDM_data = [CDM_data]
 
                     all_features = []
-                    for j, CDM_data in enumerate(all_CDM_data):
+                    for ii, data_chunk_1 in enumerate(all_CDM_data):
+                        print(f'Computing features for CDM data chunk {ii+1}/{len(all_CDM_data)}')
+
+                        # Computation of EEGs
                         if compute_EEG:
-                            all_data = np.zeros((20,len(CDM_data),len(CDM_data[0])))
-                            for sample,CDM in enumerate(CDM_data):
-                                # print progress
-                                print(f'Computing EEGs: {sample}/{len(CDM_data)}', end='\r')
-                                EEGs = potential.compute_EEG(CDM)
-                                for i, EEG in enumerate(EEGs):
-                                    all_data[i,sample,:] = EEG
-                                del EEGs
-                            all_data = all_data.tolist()
+                            all_data = []
+                            for k,CDM_data in enumerate(data_chunk_1):
+                                print(f'EEG {k+1}/{len(data_chunk_1)}', end='\r')
+                                EEGs = potential.compute_EEG(CDM_data)
+                                all_data.append(EEGs)
                         else:
-                            all_data = [CDM_data]
+                            all_data = [data_chunk_1]
 
                         # Get the features for each chunk
-                        for i, data_chunk in enumerate(all_data):
-                            print(f'Chunk {i+1}/{len(all_data)} of CDM_data {j+1}/{len(all_CDM_data)}')
+                        for jj, data_chunk_2 in enumerate(all_data):
+                            print(f'Chunk {jj+1}/{len(all_data)} of CDM_data {ii+1}/{len(all_CDM_data)}')
                             # Create a fake Pandas DataFrame (only Data and fs are relevant)
-                            df = pd.DataFrame({'ID': np.zeros(len(data_chunk)),
-                                               'Group': np.arange(len(data_chunk)),
-                                               'Epoch': np.zeros(len(data_chunk)),
-                                               'Sensor': np.zeros(len(data_chunk)),
-                                               'Data': list(data_chunk)})
+                            df = pd.DataFrame({'ID': np.zeros(len(data_chunk_2)),
+                                               'Group': np.arange(len(data_chunk_2)),
+                                               'Epoch': np.zeros(len(data_chunk_2)),
+                                               'Sensor': np.zeros(len(data_chunk_2)),
+                                               'Data': list(data_chunk_2)})
                             df.Recording = 'EEG' if compute_EEG else 'CDM'
                             df.fs = 1000. / 0.625 # samples/s
 
@@ -119,15 +120,16 @@ if __name__ == '__main__':
 
                     if compute_EEG:
                         for i in range(20):
-                            df = pd.concat([all_features[j*20+i] for j in range(len(all_CDM_data))])
+                            elec_data = []
+                            for j in range(len(all_features)):
+                                elec_data.append(all_features[j]['Features'][i])
 
-                            # Save the features to a file
-                            pickle.dump(np.array(df['Features'].tolist()),
-                                        open(os.path.join(features_path, method, 'tmp',
-                                                          'sim_X_'+file.split('_')[-1]+'_'+str(i)), 'wb'))
+                                # Save the features to a file
+                                pickle.dump(np.array(elec_data),open(os.path.join(features_path, method, 'tmp',
+                                                              'sim_X_'+file.split('_')[-1]+'_'+str(i)), 'wb'))
 
                     else:
-                        df = pd.concat(all_features)
+                        df = all_features[0]
 
                         # Save the features to a file
                         pickle.dump(np.array(df['Features'].tolist()),

ncpi/FieldPotential.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class FieldPotential:
-    def __init__(self, kernel=True, nyhead=False):
+    def __init__(self, kernel=True, nyhead=False, CDM_shape=15500):
         """
         Initialize the FieldPotential object.
         Parameters
@@ -20,6 +20,9 @@ class FieldPotential:
 
         nyhead: bool
             Use the NYHeadModel for computing EEG.
+
+        CDM_shape: int
+            Number of time points in the CDM.
         """
         # Initialize dictionary for storing kernels
         if kernel:
@@ -28,6 +31,9 @@ class FieldPotential:
         # Initialize the head model
         if nyhead:
             self.nyhead = NYHeadModel()
+            # Allocate memory for the transformation matrix and EEG
+            self.M = np.zeros((231,3))
+            self.EEG = np.zeros((231,CDM_shape))
 
     def create_kernel(self, MC_folder, output_path, params, biophys, dt, tstop, electrodeParameters=None, CDM=True):
         """
@@ -217,28 +223,28 @@ class FieldPotential:
         p = np.zeros((3, len(CDM)))
         p[2, :] = CDM
 
-        all_EEG = []
         # If location is provided, compute EEG at that location
         if location is not None:
             # Set the dipole location
             self.nyhead.set_dipole_pos(location)
 
             # Get the transformation matrix
-            M = self.nyhead.get_transformation_matrix()
+            self.M = self.nyhead.get_transformation_matrix()
 
             # Rotate current dipole moment to be oriented along the normal vector of cortex
             p = self.nyhead.rotate_dipole_to_surface_normal(p)
 
             # Compute EEG
-            EEG = M @ p  # (mV)
+            self.EEG = self.M @ p  # (mV)
 
             # Get the closest electrode idx to dipole location
             dist, closest_elec_idx = self.nyhead.find_closest_electrode()
-            all_EEG.append(EEG[closest_elec_idx, :])
+            all_EEG = self.EEG[closest_elec_idx, :]
 
         # If location is not provided, place the dipole at the different locations of the 10-20 EEG setup. We assume
         # that each dipole is working independently, i.e., the EEG at each electrode is computed separately.
         else:
+            all_EEG = np.zeros((20,self.EEG.shape[1]))
             locations = [np.array([-25, 65, 0]), # Fp1
                         np.array([25, 65, 0]),  # Fp2
                         np.array([-50, 36, -10]), # F7
@@ -304,25 +310,25 @@ class FieldPotential:
 
                 plt.show()
 
-            for location in locations:
+            for ii,location in enumerate(locations):
                 # Set the dipole location
                 self.nyhead.set_dipole_pos(location)
 
                 # Get the transformation matrix
-                M = self.nyhead.get_transformation_matrix()
+                self.M = self.nyhead.get_transformation_matrix()
 
                 # Rotate current dipole moment to be oriented along the normal vector of cortex
                 p = self.nyhead.rotate_dipole_to_surface_normal(p)
 
                 # Compute EEG
-                EEG = M @ p  # (mV)
+                self.EEG = self.M @ p  # (mV)
 
                 # Get the closest electrode idx to dipole location
                 dist, closest_elec_idx = self.nyhead.find_closest_electrode()
-                all_EEG.append(EEG[closest_elec_idx, :])
+                all_EEG[ii,:] = self.EEG[closest_elec_idx, :]
 
             # Delete variables
-            del M, p, EEG
+            # del p, dist, closest_elec_idx
 
         return all_EEG
 