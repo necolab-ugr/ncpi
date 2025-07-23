8003de66652144584ed53ad61846bdebe69c9087
Pablo Martínez Cañada <pablomc@ugr.es>
2025-07-23 08:12:06 +0200

    Add reference to related project where E/I is directly modeled with SBI

--- examples/simulation/Hagen_model/figures/SBI_results.py
+++ examples/simulation/Hagen_model/figures/SBI_results.py
@@ -228,7 +228,11 @@ if compute_metrics:
                     fold = int(10. * sample / theta_EI.shape[0])
                     posterior_samples = [posterior[fold].sample((n_post_samples,), x=x_o, show_progress_bars=False)]
 
-                # Calculate E/I
+                # Compute E/I by dividing the posterior distributions and averaging the sorted samples across folds.
+                # Note: This approach may not be the most straightforward or appropriate.
+                # In a related study (see https://github.com/necolab-ugr/SBI_mechanistic_modeling_AD), 
+                # we trained SBI models directly on the E/I ratio, which offers a more suitable and principled method.
+                
                 new_post_samples = [np.zeros((posterior_samples[0].shape[0],
                                               4)) for _ in range(len(posterior_samples))]
                 for ii in range(len(posterior_samples)):
@@ -255,13 +259,18 @@ if compute_metrics:
                 p = np.zeros(4)
                 try:
                     for param in range(4):
-                        # Remove low-probability samples to prevent distributions with long tails
+                        # Remove low-probability samples to prevent distributions with long tails:
+                        # We observed that calculating E/I as the ratio of posterior distributions over the J_YX parameters 
+                        # resulted in long-tailed distributions with low probability mass—an artifact of the indirect approach. 
+                        # This workaround was unnecessary when training SBI models to predict E/I directly, as done in the related 
+                        # project: https://github.com/necolab-ugr/SBI_mechanistic_modeling_AD.
+                        
                         hist, bin_edges = np.histogram(avg_post_samples[:,param],
                                                        bins=np.linspace(lims[param][0], lims[param][1], 1000),
                                                        density=True)
                         hist = gaussian_filter1d(hist, sigma=5)
                         hist /= np.max(hist)
-                        iii = bin_edges[np.where(hist > 0.5)[0]]
+                        iii = bin_edges[np.where(hist > 0.25)[0]]
                         pos = np.where((avg_post_samples[:,param] > iii[0]) & (avg_post_samples[:,param] <= iii[-1]))[0]
                         param_post_samples = avg_post_samples[pos,param]
 
