e75e6fa790e517b6aeed7b9afb34e6b637ea49f4
Laura <laurator@ugr.es>
2025-10-28 12:46:25 +0100

    Same code, more modularity. Included use of timer

--- examples/LFP_developing_brain/LFP_developing_brain.py
+++ examples/LFP_developing_brain/LFP_developing_brain.py
@@ -7,6 +7,7 @@ import numpy as np
 import time
 import ncpi
 from ncpi import tools
+from ncpi.tools import timer
 
 # Choose to either download data from Zenodo (True) or load it from a local path (False).
 # Important: the zenodo downloads will take a while, so if you have already downloaded the data, set this to False and
@@ -30,6 +31,182 @@ all_methods = ['catch22','power_spectrum_parameterization_1']
 # ML model used to compute the predictions (MLPRegressor, Ridge or NPE)
 ML_model = 'MLPRegressor'
 
+
+@timer("Loading simulation data.")
+def load_model_features(method, zenodo_dir_sim):
+    # Load parameters of the model (theta) and features (X) from simulation data
+    try:
+        with open(os.path.join(zenodo_dir_sim, 'data', method, 'sim_theta'), 'rb') as file:
+            theta = pickle.load(file)
+        with open(os.path.join(zenodo_dir_sim, 'data', method, 'sim_X'), 'rb') as file:
+            X = pickle.load(file)
+    except Exception as e:
+        print(f"Error loading simulation data: {e}")
+
+    # Print info
+    print('theta:')
+    for key, value in theta.items():
+        if isinstance(value, np.ndarray):
+            print(f'--Shape of {key}: {value.shape}')
+        else:
+            print(f'--{key}: {value}')
+    print(f'Shape of X: {X.shape}')
+
+    return X, theta
+
+
+@timer("Loading the inverse model.")
+def load_inference_data(method, X, theta, zenodo_dir_sim):
+    # Load the Inference objects and add the simulation data
+
+    # Create inference object
+    inference = ncpi.Inference(model=ML_model)
+    # Not sure if this is really needed
+    inference.add_simulation_data(X, theta['data'])
+
+    # Create folder to save results
+    if not os.path.exists('data'):
+        os.makedirs('data')
+    if not os.path.exists(os.path.join('data', method)):
+        os.makedirs(os.path.join('data', method))
+
+    # Transfer model and scaler to the data folder
+    if ML_model == 'MLPRegressor':
+        folder = 'MLP'
+    elif ML_model == 'Ridge':
+        folder = 'Ridge'
+    elif ML_model == 'NPE':
+        folder = 'SBI'
+        
+    shutil.copy(
+        os.path.join(zenodo_dir_sim, 'ML_models/4_param', folder, method, 'scaler'),
+        os.path.join('data', 'scaler.pkl')
+    )
+
+    shutil.copy(
+        os.path.join(zenodo_dir_sim, 'ML_models/4_param', folder, method, 'model'),
+        os.path.join('data', 'model.pkl')
+    )
+
+    if ML_model == 'NPE':
+        shutil.copy(
+            os.path.join(zenodo_dir_sim, 'ML_models/4_param', folder, method,
+                            'density_estimator'),
+            os.path.join('data', 'density_estimator.pkl')
+        )
+
+    return inference
+
+
+@timer("Loading LFP data.")
+def load_empirical_data(zenodo_dir_emp):
+    # Load empirical data
+    file_list = os.listdir(os.path.join(zenodo_dir_emp, 'development_EI_decorrelation/baseline/LFP'))
+    emp_data = {'LFP': [], 'fs': [], 'age': []}
+
+    for i,file_name in enumerate(file_list):
+        print(f'\r Progress: {i+1} of {len(file_list)} files loaded', end='', flush=True)
+        structure = scipy.io.loadmat(os.path.join(os.path.join(zenodo_dir_emp,
+                                                                'development_EI_decorrelation/baseline/LFP'),
+                                                    file_name))
+        LFP = structure['LFP']['LFP'][0,0]
+        sum_LFP = np.sum(LFP, axis=0)  # sum LFP across channels
+        fs = structure['LFP']['fs'][0, 0][0, 0]
+        age = structure['LFP']['age'][0,0][0,0]
+
+        emp_data['LFP'].append(sum_LFP)
+        emp_data['fs'].append(fs)
+        emp_data['age'].append(age)
+
+    print(f'\nFiles loaded: {len(emp_data["LFP"])}')
+
+    return emp_data
+
+
+@timer("Computing features from empirical data.") 
+def compute_features_empirical_data(method, emp_data):
+    # Compute features from empirical data
+
+    # Epoch length in seconds
+    chunk_size = 5
+
+    # Parameters of the feature extraction method
+    if method == 'catch22':
+        params = None
+    elif method == 'power_spectrum_parameterization_1':
+        fooof_setup_emp = {'peak_threshold': 1.,
+                            'min_peak_height': 0.,
+                            'max_n_peaks': 5,
+                            'peak_width_limits': (10., 50.)}
+        params={'fs': emp_data['fs'][0],
+                'fmin': 5.,
+                'fmax': 45.,
+                'fooof_setup': fooof_setup_emp,
+                'r_squared_th':0.9}
+
+    # Split the data into chunks (epochs)
+    chunk_size = int(chunk_size * emp_data['fs'][0])
+    chunked_data = []
+    ID = []
+    epoch = []
+    group = []
+    for i in range(len(emp_data['LFP'])):
+        for e,j in enumerate(range(0, len(emp_data['LFP'][i]), chunk_size)):
+            if len(emp_data['LFP'][i][j:j+chunk_size]) == chunk_size:
+                chunked_data.append(emp_data['LFP'][i][j:j+chunk_size])
+                ID.append(i)
+                epoch.append(e)
+                group.append(emp_data['age'][i])
+
+    # Create the Pandas DataFrame
+    df = pd.DataFrame({'ID': ID,
+                        'Group': group,
+                        'Epoch': epoch,
+                        'Sensor': np.zeros(len(ID)), # dummy sensor
+                        'Data': chunked_data})
+    df.Recording = 'LFP'
+    df.fs = emp_data['fs'][0]
+
+    # Compute features
+    features = ncpi.Features(method=method if method == 'catch22' else 'power_spectrum_parameterization',
+                                params=params)
+    emp_data = features.compute_features(df)
+
+    # Keep only the aperiodic exponent (1/f slope)
+    if method == 'power_spectrum_parameterization_1':
+        emp_data['Features'] = emp_data['Features'].apply(lambda x: x[1])
+
+    return emp_data
+
+
+@timer("Computing predictions from empirical data.")
+def compute_predictions_empirical_data(emp_data, inference):
+    # Compute predictions from the empirical data
+
+    # Predict the parameters from the features of the empirical data
+    predictions = inference.predict(np.array(emp_data['Features'].tolist()))
+
+    # Append the predictions to the DataFrame
+    pd_preds = pd.DataFrame({'Predictions': predictions})
+    emp_data = pd.concat([emp_data, pd_preds], axis=1)
+
+    return emp_data
+
+def save_data(emp_data, method):
+    # Save the data including predictions of all parameters
+    emp_data.to_pickle(os.path.join('data', method, 'emp_data_all.pkl'))
+
+    # Replace parameters of recurrent synaptic conductances with the ratio (E/I)_net
+    E_I_net = emp_data['Predictions'].apply(lambda x: (x[0]/x[2]) / (x[1]/x[3]))
+    others = emp_data['Predictions'].apply(lambda x: x[4:])
+    emp_data['Predictions'] = (np.concatenate((E_I_net.values.reshape(-1,1),
+                                                np.array(others.tolist())), axis=1)).tolist()
+
+    # Save the data including predictions of (E/I)_net
+    emp_data.to_pickle(os.path.join('data', method, 'emp_data_reduced.pkl'))
+
+
+
 if __name__ == "__main__":
     # Download simulation data and ML models
     if zenodo_dw_sim:
@@ -52,173 +229,19 @@ if __name__ == "__main__":
         print(f'\n\n--- Method: {method}')
 
         # Load parameters of the model (theta) and features (X) from simulation data
-        print('\n--- Loading simulation data.')
-        start_time = time.time()
-
-        try:
-            with open(os.path.join(zenodo_dir_sim, 'data', method, 'sim_theta'), 'rb') as file:
-                theta = pickle.load(file)
-            with open(os.path.join(zenodo_dir_sim, 'data', method, 'sim_X'), 'rb') as file:
-                X = pickle.load(file)
-        except Exception as e:
-            print(f"Error loading simulation data: {e}")
-
-        # Print info
-        print('theta:')
-        for key, value in theta.items():
-            if isinstance(value, np.ndarray):
-                print(f'--Shape of {key}: {value.shape}')
-            else:
-                print(f'--{key}: {value}')
-        print(f'Shape of X: {X.shape}')
-
-        end_time = time.time()
-        print(f"Done in {(end_time - start_time):.2f} sec.")
+        X, theta = load_model_features(method, zenodo_dir_sim)
 
         # Load the Inference objects and add the simulation data
-        print('\n--- Loading the inverse model.')
-        start_time = time.time()
-
-        # Create inference object
-        inference = ncpi.Inference(model=ML_model)
-        # Not sure if this is really needed
-        inference.add_simulation_data(X, theta['data'])
-
-        # Create folder to save results
-        if not os.path.exists('data'):
-            os.makedirs('data')
-        if not os.path.exists(os.path.join('data', method)):
-            os.makedirs(os.path.join('data', method))
-
-        # Transfer model and scaler to the data folder
-        if ML_model == 'MLPRegressor':
-            folder = 'MLP'
-        elif ML_model == 'Ridge':
-            folder = 'Ridge'
-        elif ML_model == 'NPE':
-            folder = 'SBI'
-            
-        shutil.copy(
-            os.path.join(zenodo_dir_sim, 'ML_models/4_param', folder, method, 'scaler'),
-            os.path.join('data', 'scaler.pkl')
-        )
-
-        shutil.copy(
-            os.path.join(zenodo_dir_sim, 'ML_models/4_param', folder, method, 'model'),
-            os.path.join('data', 'model.pkl')
-        )
-
-        if ML_model == 'NPE':
-            shutil.copy(
-                os.path.join(zenodo_dir_sim, 'ML_models/4_param', folder, method,
-                             'density_estimator'),
-                os.path.join('data', 'density_estimator.pkl')
-            )
-
-        end_time = time.time()
-        print(f'Done in {(end_time - start_time):.2f} sec')
+        inference = load_inference_data(method, X, theta, zenodo_dir_sim)
 
         # Load empirical data
-        print('\n--- Loading LFP data.')
-        start_time = time.time()
-
-        file_list = os.listdir(os.path.join(zenodo_dir_emp, 'development_EI_decorrelation/baseline/LFP'))
-        emp_data = {'LFP': [], 'fs': [], 'age': []}
-
-        for i,file_name in enumerate(file_list):
-            print(f'\r Progress: {i+1} of {len(file_list)} files loaded', end='', flush=True)
-            structure = scipy.io.loadmat(os.path.join(os.path.join(zenodo_dir_emp,
-                                                                   'development_EI_decorrelation/baseline/LFP'),
-                                                      file_name))
-            LFP = structure['LFP']['LFP'][0,0]
-            sum_LFP = np.sum(LFP, axis=0)  # sum LFP across channels
-            fs = structure['LFP']['fs'][0, 0][0, 0]
-            age = structure['LFP']['age'][0,0][0,0]
-
-            emp_data['LFP'].append(sum_LFP)
-            emp_data['fs'].append(fs)
-            emp_data['age'].append(age)
-
-        end_time = time.time()
-        print(f'\nFiles loaded: {len(emp_data["LFP"])}')
-        print(f'Done in {(end_time - start_time):.2f} sec')
+        emp_data = load_empirical_data(zenodo_dir_emp)
 
         # Compute features from empirical data
-        print('\n--- Computing features from empirical data.')
-        start_time = time.time()
-
-        # Epoch length in seconds
-        chunk_size = 5
-
-        # Parameters of the feature extraction method
-        if method == 'catch22':
-            params = None
-        elif method == 'power_spectrum_parameterization_1':
-            fooof_setup_emp = {'peak_threshold': 1.,
-                               'min_peak_height': 0.,
-                               'max_n_peaks': 5,
-                               'peak_width_limits': (10., 50.)}
-            params={'fs': emp_data['fs'][0],
-                   'fmin': 5.,
-                   'fmax': 45.,
-                   'fooof_setup': fooof_setup_emp,
-                   'r_squared_th':0.9}
-
-        # Split the data into chunks (epochs)
-        chunk_size = int(chunk_size * emp_data['fs'][0])
-        chunked_data = []
-        ID = []
-        epoch = []
-        group = []
-        for i in range(len(emp_data['LFP'])):
-            for e,j in enumerate(range(0, len(emp_data['LFP'][i]), chunk_size)):
-                if len(emp_data['LFP'][i][j:j+chunk_size]) == chunk_size:
-                    chunked_data.append(emp_data['LFP'][i][j:j+chunk_size])
-                    ID.append(i)
-                    epoch.append(e)
-                    group.append(emp_data['age'][i])
-
-        # Create the Pandas DataFrame
-        df = pd.DataFrame({'ID': ID,
-                           'Group': group,
-                           'Epoch': epoch,
-                           'Sensor': np.zeros(len(ID)), # dummy sensor
-                           'Data': chunked_data})
-        df.Recording = 'LFP'
-        df.fs = emp_data['fs'][0]
-
-        # Compute features
-        features = ncpi.Features(method=method if method == 'catch22' else 'power_spectrum_parameterization',
-                                 params=params)
-        emp_data = features.compute_features(df)
-
-        # Keep only the aperiodic exponent (1/f slope)
-        if method == 'power_spectrum_parameterization_1':
-            emp_data['Features'] = emp_data['Features'].apply(lambda x: x[1])
-
-        end_time = time.time()
-        print(f'Done in {(end_time - start_time):.2f} sec')
+        emp_data = compute_features_empirical_data(method, emp_data)
 
         # Compute predictions from the empirical data
-        print('\n--- Computing predictions from empirical data.')
-        start_time = time.time()
-        # Predict the parameters from the features of the empirical data
-        predictions = inference.predict(np.array(emp_data['Features'].tolist()))
+        emp_data = compute_predictions_empirical_data(emp_data, inference)
 
-        # Append the predictions to the DataFrame
-        pd_preds = pd.DataFrame({'Predictions': predictions})
-        emp_data = pd.concat([emp_data, pd_preds], axis=1)
-        end_time = time.time()
-        print(f'Done in {(end_time - start_time)/60.} min')
-
-        # Save the data including predictions of all parameters
-        emp_data.to_pickle(os.path.join('data', method, 'emp_data_all.pkl'))
-
-        # Replace parameters of recurrent synaptic conductances with the ratio (E/I)_net
-        E_I_net = emp_data['Predictions'].apply(lambda x: (x[0]/x[2]) / (x[1]/x[3]))
-        others = emp_data['Predictions'].apply(lambda x: x[4:])
-        emp_data['Predictions'] = (np.concatenate((E_I_net.values.reshape(-1,1),
-                                                   np.array(others.tolist())), axis=1)).tolist()
-
-        # Save the data including predictions of (E/I)_net
-        emp_data.to_pickle(os.path.join('data', method, 'emp_data_reduced.pkl'))
+        # Save the data including predictions of all parameters and of (E/I)_net
+        save_data(emp_data, method)
