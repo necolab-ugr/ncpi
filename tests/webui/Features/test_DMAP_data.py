5d3eb8aec96329bc03de7ba63b667712eab08bd7
Laura <laurator@ugr.es>
2026-04-30 07:51:15 +0200

    fix: Fix recording type value disable state by forcing select re-sync after parser info changes

---
+++ tests/webui/Features/test_DMAP_data.py
@@ -0,0 +1,58 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False, slow_mo=150)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("http://localhost:5000/features/methods?entry=compute")
+    page.get_by_role("button", name="add_circle New data Upload or").click()
+    page.get_by_role("button", name="Add server folder").click()
+    page.get_by_role("button", name="Up").click()
+    page.get_by_role("button", name="Up").click()
+    page.get_by_role("button", name="DATOS").click()
+    page.get_by_role("button", name="pablomc").click()
+    page.get_by_role("button", name="empirical_datasets").click()
+    page.get_by_role("button", name="DMAP_data").click()
+    page.get_by_role("button", name="Add this folder").click()
+    page.get_by_role("button", name="Server files").click()
+    page.get_by_role("button", name="Browse server files").click()
+    page.get_by_role("button", name="Up", exact=True).click()
+    page.get_by_role("button", name="Up", exact=True).click()
+    page.get_by_role("button", name="folder DATOS").click()
+    page.get_by_role("button", name="folder pablomc").click()
+    page.get_by_role("button", name="folder empirical_datasets").click()
+    page.get_by_role("button", name="folder DMAP_data").click()
+    page.get_by_role("button", name="description dkatlas_regionlabels.csv").click()
+    page.get_by_label("Data extension used as source").select_option("__ext__:.mat")
+    page.get_by_label("Data locator * Preferred:").first.select_option("")
+    page.get_by_label("Data locator * Preferred:").first.select_option("pcai_matrix")
+    page.get_by_role("checkbox", name="Specify array dimension").check()
+    page.get_by_label("Channels axis None Dim 0 Dim").first.select_option("1")
+    page.get_by_label("Samples axis Dim 0 Dim 1 Dim").first.select_option("0")
+    page.get_by_label("IDs axis None Dim 0 Dim 1 Dim").first.select_option("2")
+    page.get_by_label("Trials/Epochs axis None Dim 0").first.select_option("3")
+    page.get_by_label("Recording type value LFP CDM").first.select_option("EEG")
+    page.get_by_label("Subject ID source None Custom").select_option("sub_IDs")
+    page.get_by_label("Group source None Custom").select_option("label")
+    page.get_by_role("checkbox", name="Enable epoching").check()
+    page.get_by_role("button", name="Next: Select method").click()
+    page.get_by_role("radio", name="Catch22 22 canonical time-").check()
+    page.get_by_role("button", name="Next step arrow_forward").click()
+    page.get_by_role("checkbox", name="Subsampling of data").check()
+    page.get_by_role("slider").fill("5")
+    page.get_by_role("spinbutton", name="Parallel workers (n_jobs)").click()
+    page.get_by_role("spinbutton", name="Parallel workers (n_jobs)").fill("48")
+    expect(page.get_by_role("button", name="Compute features arrow_forward")).to_be_visible(timeout= 900000)
+    page.get_by_role("button", name="Compute features arrow_forward").click()
+    expect(page.get_by_role("main")).to_contain_text("Features Computed", timeout=2000000)
+    expect(page.get_by_role("link", name="Continue to Inference")).to_be_visible(timeout=2000000)
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
