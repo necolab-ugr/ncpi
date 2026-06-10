8374b0f902610b236bda9af378f3fc1d648b4f4b
alejandroov <alexoroval@ugr.es>
2026-06-10 15:53:08 +0200

    tests: support latests specparam import path

--- tests/Features/test_specparam.py
+++ tests/Features/test_specparam.py
@@ -6,7 +6,11 @@ import scipy.signal as scipy_signal
 specparam = pytest.importorskip("specparam")
 from specparam import SpectralModel
 from specparam.sim import sim_power_spectrum
-from specparam.sim.utils import set_random_seed
+
+try:
+    from specparam.utils.random import set_random_seed
+except:
+    from specparam.sim.utils import set_random_seed
 
 from ncpi.Features import Features
 
